c480b9b1aa1d7429a80131c0a43e995babf94623
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-01-22 16:56:44 -0500

    added seed info, added prompt, and connection

---
+++ seeds.sql
@@ -0,0 +1,33 @@
+INSERT INTO department (name)
+VALUES 
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
+
+
+
+
+
+
+INSERT INTO role (title, salary, role_id)
+VALUES
+('Sales Lead, 30000, 1'),
+('Salesperson, 25000, 2'),
+('Lead Engineer, 50000, 3'),
+('Software Engineer, 40000, 4'),
+('Accountant, 100000, 5'),
+('Legal Team, 90000, 6'),
+('Lawyer, 150000, 7');
+
+
+
+
+INSERT INTO employee (first_name, last_name, role_name, manager)
+VALUES
+('Michael','Pinto'),
+('Kendra', 'Timot'),
+('Joyce', 'Carter'),
+('Tomeshia', 'Williamson'),
+('Shay', 'Lowe'),
+('Anthony', 'Lewis'),
--- tracker.js
+++ tracker.js
@@ -1,1 +1,46 @@
 const inquirer = require('inquirer');
+
+
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    // Your MySQL username
+    user: 'root',
+    // Your MySQL password
+    password: 'Bereal21!',
+    database: ''
+  });
+
+  connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    startPrompt();
+  });
+  
+
+
+function startPrompt() {
+    inquirer.prompt ([
+        {
+
+            typer: "list",
+            message: "What would you like to do?",
+            name: "",
+            choices: [
+                "Add department",
+                "Add employee",
+                "Add role",
+                "Remove employee",
+                "Add role",
+                "Remove employee",
+                "Update employee role",
+                "View all departments",
+                "View all employees",
+                "View all employee by department",
+                "View all roles",
+            ]
+        }
+    ])
+
+}
