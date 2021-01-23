e4174518357b4b3d8dbc31ec31a3d999347ed105
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-01-23 18:45:37 -0500

    added prompt question, updated seed info

--- seeds.sql
+++ seeds.sql
@@ -10,24 +10,24 @@ VALUES
 
 
 
-INSERT INTO role (title, salary, role_id)
+INSERT INTO role (title, salary, department_id)
 VALUES
-('Sales Lead, 30000, 1'),
-('Salesperson, 25000, 2'),
-('Lead Engineer, 50000, 3'),
-('Software Engineer, 40000, 4'),
-('Accountant, 100000, 5'),
-('Legal Team, 90000, 6'),
-('Lawyer, 150000, 7');
+('Sales Lead', 30000, 1),
+('Salesperson', 25000, 1),
+('Lead Engineer', 50000, 2),
+('Software Engineer', 40000, 2),
+('Accountant', 100000, 3),
+('Legal Team Lead', 90000, 3),
+('Lawyer', 150000, 4);
 
 
 
 
-INSERT INTO employee (first_name, last_name, role_name, manager)
+INSERT INTO employee (first_name, last_name, role_id, manager_ID)
 VALUES
-('Michael','Pinto', 'Lawyer', null),
-('Kendra', 'Timot', 'Accountant, Johnathan Smit'),
-('Joyce', 'Carter', 'Software Engineer, null'),
-('Tomeshia', 'Williamson', 'Lead Engineer, Martha Smith'),
-('Shay', 'Lowe', 'Sales Lead, Mike Scott'),
-('Anthony', 'Lewis', 'Salesperson', 'Andrew Wood'),
+('Michael','Pinto', 1, null),
+('Kendra', 'Timot', 2, null),
+('Joyce', 'Carter', 3, null),
+('Tomeshia', 'Williamson', 4, 2),
+('Shay', 'Lowe', 5, 1),
+('Anthony', 'Lewis', 6, 3);
--- tracker.js
+++ tracker.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
 
 // Create the connection to database
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
     user: 'root',
     // Your MySQL password
     password: 'Bereal21!',
-    database: ''
+    database: 'employeedb'
   });
 
   connection.connect(err => {
@@ -25,7 +25,7 @@ function startPrompt() {
     inquirer.prompt ([
         {
 
-            typer: "list",
+            type: "list",
             message: "What would you like to do?",
             name: "choices",
             choices: [
@@ -64,4 +64,99 @@ function startPrompt() {
             }
     })
 
+}
+
+function viewAllDepartments () {
+    let query = 'SELECT * FROM DEPARTMENTS';
+    connection.query (query, function (err,res) {
+    if (err) throw err;
+
+ 
+
+    })
+}
+
+function viewAllRoles() {
+    let query = 'SELECT * FROM ROLES';
+    connection.query (query, function (err,res) {
+    if (err) throw err;    
+
+    })
+}
+
+
+function viewAllEmployees() {
+    let query = 'SELECT * FROM EMPLOYEE';
+    connection.query (query, function (err,res) {
+    if (err) throw err;   
+}
+
+function addDepartment() {
+    
+    inquirer.prompt ([
+        {
+            type:'text',
+            name:'department',
+            message: 'What department would you like to add?'
+        }
+    ])   
+
+
+function addEmployee() {
+    connection.query (, function (err,res) {
+    if (err) throw err;
+
+    inquirer.prompt([
+    {
+        type: 'text',
+        name: 'name',
+        message: 'What is the employee first name?'
+
+    },
+    {
+        type: 'text',
+        name: 'name',
+        message: 'What is the employee last name?'
+    }
+])
+
+function addRole() {
+    connection.query (, function (err,res) {
+    if (err) throw err;
+
+    inquirer.prompt ([
+        {
+            type:'text',
+            name:'role',
+            message: 'What role would you like to add?'
+        },
+        {
+            type: 'text',
+            name:'salary',
+            message: 'What is the role salary?'
+        },
+        {
+            type:'text',
+            name:'Department id',
+            message: 'What is the department id?'
+        }
+    ])
+
+    
+function updateEmployee() {
+    connection.query (, function (err,res) {
+    if (err) throw err;    
+
+    inquirer.prompt ([
+        {
+            type:'text',
+            name:'update employee',
+            message: 'What employee would you like to update?'
+        },
+        {
+            type:'text',
+            name:'update role',
+            message: 'What role would you like to change it to?'
+        }
+    ])
 }
